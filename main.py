d28272bb76cb5ab478600b1887e2a9a88d18ca3c
suryaremanan <ksuryaremanan@gmail.com>
2023-03-18 23:33:39 +0530

    second commit

--- main.py
+++ main.py
@@ -4,4 +4,4 @@ from ultralytics import YOLO
 model = YOLO("yolov8n.yaml")  # build a new model from scratch
 
 # Use the model
-results = model.train(data="config.yaml", epochs=100)  # train the model
+results = model.train(data="config.yaml", epochs=5000)  # train the model
